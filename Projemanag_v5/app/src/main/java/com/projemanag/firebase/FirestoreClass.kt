f48761dfaf367a6415d9a3246579c73e7d69b67a
Chetanpatel57 <40812586+Chetanpatel57@users.noreply.github.com>
2023-04-03 16:10:18 -0400

    Database connectivity added

---
+++ Projemanag_v5/app/src/main/java/com/projemanag/firebase/FirestoreClass.kt
@@ -0,0 +1,46 @@
+package com.projemanag.firebase
+
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.SetOptions
+import com.projemanag.activities.SignUpActivity
+import com.projemanag.model.User
+import com.projemanag.utils.Constants
+
+class FirestoreClass {
+    // Create a instance of Firebase Firestore
+    private val mFireStore = FirebaseFirestore.getInstance()
+
+    /**
+     * A function to make an entry of the registered user in the firestore database.
+     */
+    fun registerUser(activity: SignUpActivity, userInfo: User) {
+
+        mFireStore.collection(Constants.USERS)
+            // Document ID for users fields. Here the document it is the User ID.
+            .document(getCurrentUserID())
+            // Here the userInfo are Field and the SetOption is set to merge. It is for if we wants to merge
+            .set(userInfo, SetOptions.merge())
+            .addOnSuccessListener {
+
+                // Here call a function of base activity for transferring the result to it.
+                activity.userRegisteredSuccess()
+            }
+            .addOnFailureListener { e ->
+                Log.e(
+                    activity.javaClass.simpleName,
+                    "Error writing document",
+                    e
+                )
+            }
+    }
+
+    // START
+    /**
+     * A function for getting the user id of current logged user.
+     */
+    fun getCurrentUserID(): String {
+        return FirebaseAuth.getInstance().currentUser!!.uid
+    }
+}
