8748d07296a819282ec3c8e30365b0e12a2b3c51
Chetanpatel57 <40812586+Chetanpatel57@users.noreply.github.com>
2023-04-04 20:52:29 -0400

    profile activity added

--- Project_Manager/app/src/main/java/com/projemanag/activities/MainActivity.kt
+++ Project_Manager/app/src/main/java/com/projemanag/activities/MainActivity.kt
@@ -3,11 +3,16 @@ package com.projemanag.activities
 import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.core.view.GravityCompat
+import com.bumptech.glide.Glide
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.projemanag.R
+import com.projemanag.firebase.FirestoreClass
+import com.projemanag.model.User
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 
@@ -20,14 +25,12 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
         // This is used to align the xml view to this class
         setContentView(R.layout.activity_main)
 
-        // START
-        setupActionBar()
-        // END
 
-        // START
-        // Assign the NavigationView.OnNavigationItemSelectedListener to navigation view.
+        setupActionBar()
+
         nav_view.setNavigationItemSelectedListener(this)
-        // END
+
+        FirestoreClass().signInUser(this@MainActivity)
     }
 
     // START
@@ -47,7 +50,7 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
         when (menuItem.itemId) {
             R.id.nav_my_profile -> {
 
-                Toast.makeText(this@MainActivity, "My Profile", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this@MainActivity, MyProfileActivity::class.java))
             }
 
             R.id.nav_sign_out -> {
@@ -92,4 +95,24 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
         }
     }
     // END
+    fun updateNavigationUserDetails(user: User) {
+        // The instance of the header view of the navigation view.
+        val headerView = nav_view.getHeaderView(0)
+
+        // The instance of the user image of the navigation view.
+        val navUserImage = headerView.findViewById<ImageView>(R.id.iv_user_image)
+
+        // Load the user image in the ImageView.
+        Glide
+            .with(this@MainActivity)
+            .load(user.image) // URL of the image
+            .centerCrop() // Scale type of the image.
+            .placeholder(R.drawable.ic_user_place_holder) // A default place holder
+            .into(navUserImage) // the view in which the image will be loaded.
+
+        // The instance of the user name TextView of the navigation view.
+        val navUsername = headerView.findViewById<TextView>(R.id.tv_username)
+        // Set the user name
+        navUsername.text = user.name
+    }
 }
--- Project_Manager/app/src/main/java/com/projemanag/firebase/FirestoreClass.kt
+++ Project_Manager/app/src/main/java/com/projemanag/firebase/FirestoreClass.kt
@@ -1,17 +1,16 @@
 package com.projemanag.firebase
 
+import android.app.Activity
 import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.SetOptions
+import com.projemanag.activities.MainActivity
 import com.projemanag.activities.SignInActivity
 import com.projemanag.activities.SignUpActivity
 import com.projemanag.model.User
 import com.projemanag.utils.Constants
 
-/**
- * A custom class where we will add the operation performed for the firestore database.
- */
 class FirestoreClass {
 
     // Create a instance of Firebase Firestore
@@ -38,7 +37,7 @@ class FirestoreClass {
                 }
     }
 
-    fun signInUser(activity: SignInActivity) {
+    fun signInUser(activity: Activity) {
 
         // Here we pass the collection name from which we wants the data.
         mFireStore.collection(Constants.USERS)
@@ -53,10 +52,29 @@ class FirestoreClass {
                     // Here we have received the document snapshot which is converted into the User Data model object.
                     val loggedInUser = document.toObject(User::class.java)!!
 
+                    // START
                     // Here call a function of base activity for transferring the result to it.
-                    activity.signInSuccess(loggedInUser)
+                    when (activity) {
+                        is SignInActivity -> {
+                            activity.signInSuccess(loggedInUser)
+                        }
+                        is MainActivity -> {
+                            activity.updateNavigationUserDetails(loggedInUser)
+                        }
+                        // END
+                    }
+                    // END
                 }
                 .addOnFailureListener { e ->
+                    when (activity) {
+                        is SignInActivity -> {
+                            activity.hideProgressDialog()
+                        }
+                        is MainActivity -> {
+                            activity.hideProgressDialog()
+                        }
+                        // END
+                    }
                     Log.e(
                             activity.javaClass.simpleName,
                             "Error while getting loggedIn user details",
